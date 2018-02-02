913fb1093530337468cc662caf3d211b11209129
Arthur <aconner@earthlink.net>
2018-02-02 10:55:27 -0800

    added xcode project

---
+++ ios/IrisTest/IrisTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IrisTest
+//
+//  Created by Arthur  on 2/2/18.
+//  Copyright © 2018 Arthur . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
