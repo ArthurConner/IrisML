a041a4ab7c1b0b13fca1ba0d55d1f0df39a25f33
Arthur <aconner@earthlink.net>
2018-02-06 10:07:02 -0800

    Now doing the test set resized

--- ios/IrisTest/IrisTest/ViewController.swift
+++ ios/IrisTest/IrisTest/ViewController.swift
@@ -12,6 +12,87 @@ import Vision
 import ImageIO
 
 
+struct FlowerImage : ExpressibleByStringLiteral {
+    
+    
+    
+    let name:String
+    let version:Int
+    
+    let image:CIImage
+    let orientation:CGImagePropertyOrientation
+    
+    
+    static let dataSet:[FlowerImage] = ["daisy2.jpg","daisy4.jpg","dandelion2.jpg","dandelion4.jpg","roses2.jpg","roses4.jpg",
+                                        "sunflowers2.jpg","sunflowers4.jpg","tulips2.jpg","tulips4.jpg","daisy1.jpg","daisy3.jpg","dandelion1.jpg",
+                                        "dandelion3.jpg","roses1.jpg","roses3.jpg","sunflowers1.jpg","sunflowers3.jpg","tulips1.jpg","tulips3.jpg"]
+    
+    init(_ file:String){
+        
+        
+        guard let im = UIImage(named: file), let ciImage = CIImage(image: im) else {
+            
+            fatalError("boo.\(file)")
+        }
+        
+        let minDim = min(im.size.height,im.size.width)
+        
+        let cropCI = ciImage.cropped(to: CGRect(x: (im.size.width - minDim)/2, y: (im.size.height - minDim)/2, width: minDim, height: minDim))
+        
+        let ratio = 128.0/minDim
+        
+        let filter = CIFilter(name: "CILanczosScaleTransform")!
+        filter.setValue(cropCI, forKey: "inputImage")
+        filter.setValue(ratio, forKey: "inputScale")
+        filter.setValue(1.0, forKey: "inputAspectRatio")
+        let outputImage = filter.value(forKey: "outputImage") as! CIImage
+        
+        self.image = outputImage
+        self.orientation = CGImagePropertyOrientation(im.imageOrientation)
+        self.name = (file as NSString).deletingPathExtension
+        self.version = Int((file as NSString).pathExtension) ?? 0
+        
+    }
+    
+    typealias ExtendedGraphemeClusterLiteralType = StringLiteralType
+    
+    init(unicodeScalarLiteral value: UnicodeScalarLiteralType) {
+        self.init("\(value)")
+    }
+    
+    init(extendedGraphemeClusterLiteral value: ExtendedGraphemeClusterLiteralType) {
+        self.init(value)
+    }
+    
+    init(stringLiteral value: StringLiteralType) {
+        self.init(value)
+    }
+    
+    
+}
+
+
+struct ModelExecution{
+    let start:TimeInterval
+    let stop:TimeInterval
+    let index:Int
+    let result:String
+    
+    func elapseTime()->TimeInterval{
+        return stop - start
+    }
+    
+    static func columnTitle() -> String{
+      return "flower.name,flower.version,index,start,stop,elapseTime,result"
+    }
+    
+    func columnFormat()->String{
+        let flower = FlowerImage.dataSet[index]
+        return "\(flower.name),\(flower.version),\(index),\(start),\(stop),\(elapseTime()),\(result)"
+    }
+    
+}
+
 class ViewController: UIViewController, ModelManagerDelegate {
     
     @IBOutlet weak var classificationLabel: UILabel!
@@ -20,10 +101,10 @@ class ViewController: UIViewController, ModelManagerDelegate {
     
     let irisModel = mobilnet025()
     let useRemote = false
-    var runs:[TimeInterval] = []
+    var runs:[ModelExecution] = []
     
-    var runImage:CIImage?
-    var runOr:CGImagePropertyOrientation = .up
+    var currentIndex:Int = 0
+    var startTime:TimeInterval = Date.timeIntervalSinceReferenceDate
     
     func makeModel(){
         if useRemote {
@@ -60,7 +141,7 @@ class ViewController: UIViewController, ModelManagerDelegate {
     
     
     func hasNew(model: MLModel) {
-        runs.append(Date.timeIntervalSinceReferenceDate)
+        startTime = Date.timeIntervalSinceReferenceDate
         do {
             let vis = try VNCoreMLModel(for: model)
             let request = VNCoreMLRequest(model: vis, completionHandler: { [weak self] request, error in
@@ -69,14 +150,11 @@ class ViewController: UIViewController, ModelManagerDelegate {
             request.imageCropAndScaleOption = .centerCrop
             self.classificationRequest = request
             
-            if let im = UIImage(named: "sunflower-sun-summer-yellow.jpg") {
-                
-                //we might not want to recurse this much
-                DispatchQueue.global(qos: .userInitiated).async {
-                    self.makeImage(for: im)
-                    self.updateClassifications()
-                }
+            
+            DispatchQueue.global(qos: .userInitiated).async {
+                self.updateClassifications()
             }
+            
             
         } catch {
             handleError(.runningModel,"unable to run model \(error)")
@@ -88,49 +166,42 @@ class ViewController: UIViewController, ModelManagerDelegate {
         debugPrint("oops")
     }
     
-    func makeImage(for image: UIImage){
-        
-        guard  self.runImage == nil else {
-            return
-        }
-        
-        guard let ciImage = CIImage(image: image) else { fatalError("Unable to create \(CIImage.self) from \(image).") }
-        
-        self.runImage = ciImage
-        self.runOr    = CGImagePropertyOrientation(image.imageOrientation)
-        
-    }
+    
     
     func updateClassifications() {
         
-        guard let request = self.classificationRequest, let runI = self.runImage else {
-            handleError(.missingObject, "don't have the classification request yet")
-            return
-        }
         
-        // DispatchQueue.global(qos: .userInitiated).async {
-        let handler = VNImageRequestHandler(ciImage: runI   , orientation: runOr)
-        do {
-            runs.append(Date.timeIntervalSinceReferenceDate)
-            try handler.perform([request])
-        } catch {
-            handleError(.runningModel, "Failed to perform classification.\n\(error.localizedDescription)")
+        DispatchQueue.global(qos: .userInitiated).asyncAfter(deadline: .now() + 0.1, execute: {
+            let flower = FlowerImage.dataSet[self.currentIndex]
+            
+            guard let request = self.classificationRequest else {
+                handleError(.missingObject, "don't have the classification request yet")
+                return
+            }
+            
+            // DispatchQueue.global(qos: .userInitiated).async {
+            let handler = VNImageRequestHandler(ciImage: flower.image   , orientation: flower.orientation)
+            do {
+                self.startTime = Date.timeIntervalSinceReferenceDate
+                try handler.perform([request])
+            } catch {
+                handleError(.runningModel, "Failed to perform classification.\n\(error.localizedDescription)")
+            }
         }
-        // }
+        )
     }
     
     
     func postProcess(){
-        var diffs:[TimeInterval] = []
-        var total:TimeInterval = 0
-        for i in stride(from: 4, to: runs.count-1, by: 2){
-            let add = runs[i+1] - runs[i]
-            diffs.append(add)
-            total += add
-        }
         
-        print("average \(total/Double(diffs.count))")
-        print(" runs \(diffs)")
+        let reportLines = runs.map{$0.columnFormat()}
+        let report = reportLines.joined(separator: "\n")
+        
+        let totalTime = runs.reduce(0, { $0 + $1.elapseTime()})/Double(runs.count)
+        
+        let columnTitle = ModelExecution.columnTitle()
+        print("average \(totalTime)")
+        print(" runs\n\(columnTitle)\n\(report)")
     }
     /// Updates the UI with the results of the classification.
     /// - Tag: ProcessClassifications
@@ -140,34 +211,43 @@ class ViewController: UIViewController, ModelManagerDelegate {
             self.classificationLabel.text = "Unable to classify image.\n\(error!.localizedDescription)"
             return
         }
-        runs.append(Date.timeIntervalSinceReferenceDate)
-        if runs.count < 100 {
+        
+        let stop = Date.timeIntervalSinceReferenceDate
+        
+        let result:String
+        
+        if let results = request.results {
+            
+            let classifications = results as! [VNClassificationObservation]
+            
+            if classifications.isEmpty {
+                result  = "Nothing recognized."
+            } else {
+                // Display top classifications ranked by confidence in the UI.
+                let topClassifications = classifications.prefix(2)
+                let descriptions = topClassifications.map { classification in
+                    // Formats the classification for display; e.g. "(0.37) cliff, drop, drop-off".
+                    return String(format: "  (%.2f) %@", classification.confidence, classification.identifier)
+                }
+                
+                result =  descriptions.joined(separator: ";")
+                
+            }
+        } else {
+            result =  "Unable to classify image.\n\(error!.localizedDescription)"
+            
+        }
+        
+        let exec = ModelExecution(start: startTime, stop: stop, index: currentIndex, result: result)
+        
+        runs.append(exec)
+        if runs.count < FlowerImage.dataSet.count * 16 {
+            currentIndex = (currentIndex + 1) % (FlowerImage.dataSet.count)
             updateClassifications()
         } else {
             postProcess()
         }
-        /*
-         DispatchQueue.main.async {
-         guard let results = request.results else {
-         self.classificationLabel.text = "Unable to classify image.\n\(error!.localizedDescription)"
-         return
-         }
-         // The `results` will always be `VNClassificationObservation`s, as specified by the Core ML model in this project.
-         let classifications = results as! [VNClassificationObservation]
-         
-         if classifications.isEmpty {
-         self.classificationLabel.text = "Nothing recognized."
-         } else {
-         // Display top classifications ranked by confidence in the UI.
-         let topClassifications = classifications.prefix(2)
-         let descriptions = topClassifications.map { classification in
-         // Formats the classification for display; e.g. "(0.37) cliff, drop, drop-off".
-         return String(format: "  (%.2f) %@", classification.confidence, classification.identifier)
-         }
-         self.classificationLabel.text = "Classification:\n" + descriptions.joined(separator: "\n")
-         }
-         }
-         */
+
         
     }
     
